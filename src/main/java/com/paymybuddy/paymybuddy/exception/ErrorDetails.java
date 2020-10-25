169ffdd086783c2533f481e8946b824c55da53f9
Laura <laura_habdul@hotmail.fr>
2020-10-25 10:32:57 +0100

    Refactor ErrorDetails class with lombok annotation

--- src/main/java/com/paymybuddy/paymybuddy/exception/ErrorDetails.java
+++ src/main/java/com/paymybuddy/paymybuddy/exception/ErrorDetails.java
@@ -1,41 +1,15 @@
 package com.paymybuddy.paymybuddy.exception;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@Getter(AccessLevel.PUBLIC)
+@Setter(AccessLevel.PUBLIC)
 public class ErrorDetails {
 
     private LocalDateTime timestamp;
     private String message;
     private String details;
-
-    public ErrorDetails(LocalDateTime timestamp, final String message, final String details) {
-        super();
-        this.timestamp = timestamp;
-        this.message = message;
-        this.details = details;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(final String message) {
-        this.message = message;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(final String details) {
-        this.details = details;
-    }
 }
