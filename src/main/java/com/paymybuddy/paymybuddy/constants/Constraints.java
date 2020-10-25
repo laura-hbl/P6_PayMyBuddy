50fe659f624fc52ec1143e82bd45c34973dab738
Laura <laura_habdul@hotmail.fr>
2020-10-25 10:24:16 +0100

    Add constants classes

---
+++ src/main/java/com/paymybuddy/paymybuddy/constants/Constraints.java
@@ -0,0 +1,23 @@
+package com.paymybuddy.paymybuddy.constants;
+
+public final class Constraints {
+
+    public Constraints() {
+    }
+
+    public static final int EMAIL_MAX_SIZE = 70;
+
+    public static final int PASSWORD_MIN_SIZE = 5;
+
+    public static final int PHONE_MAX_SIZE = 20;
+
+    public static final int IBAN_MIN_SIZE = 14;
+
+    public static final int IBAN_MAX_SIZE = 34;
+
+    public static final int BIC_MIN_SIZE = 8;
+
+    public static final int BIC_MAX_SIZE = 11;
+
+    public static final int DESCRIPTION_MAX_SIZE = 100;
+}
---
+++ src/main/java/com/paymybuddy/paymybuddy/constants/Fee.java
@@ -0,0 +1,11 @@
+package com.paymybuddy.paymybuddy.constants;
+
+import java.math.BigDecimal;
+
+public final class Fee {
+
+    public Fee() {
+    }
+
+    public static final BigDecimal FEE_RATE = new BigDecimal("0.005");
+}
