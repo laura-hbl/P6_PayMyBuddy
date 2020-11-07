32358f062b41a37cf6229517eed0dfa4f14856b9
Laura <laura_habdul@hotmail.fr>
2020-11-07 17:50:21 +0100

    Refactor security classes

--- src/main/java/com/paymybuddy/paymybuddy/security/MyUserDetailsService.java
+++ src/main/java/com/paymybuddy/paymybuddy/security/MyUserDetailsService.java
@@ -2,8 +2,13 @@ package com.paymybuddy.paymybuddy.security;
 
 import com.paymybuddy.paymybuddy.model.User;
 import com.paymybuddy.paymybuddy.repository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,7 +21,9 @@ import java.util.List;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
-     private final UserRepository userRepository;
+    private static final Logger LOGGER = LogManager.getLogger(MyUserDetailsService.class);
+
+    private final UserRepository userRepository;
 
     public MyUserDetailsService(final UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -24,16 +31,22 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        LOGGER.debug("Inside MyUserDetailsService.loadUserByUsername for username : " + email);
 
         User user = userRepository.findByEmail(email);
 
         if (user == null) {
+            LOGGER.error("Invalid username or password");
             throw new UsernameNotFoundException("Invalid username or password");
         }
 
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
         grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
         grantedAuthorities.add(new SimpleGrantedAuthority("ADMIN"));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, user.getEmail(),
+                grantedAuthorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return new MyUserDetails(user, grantedAuthorities);
     }
--- src/main/java/com/paymybuddy/paymybuddy/security/SecurityConfiguration.java
+++ src/main/java/com/paymybuddy/paymybuddy/security/SecurityConfiguration.java
@@ -1,22 +1,36 @@
 package com.paymybuddy.paymybuddy.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
+
+    private final ObjectMapper objectMapper;
+
+    public SecurityConfiguration(UserDetailsService userDetailsService, ObjectMapper objectMapper) {
+        this.userDetailsService = userDetailsService;
+        this.objectMapper = objectMapper;
+    }
 
     /**
      * Creates an instance of BCryptPasswordEncoder in order to encrypt the password.
@@ -28,27 +42,82 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
     @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(userDetailsService);
-        auth.setPasswordEncoder(passwordEncoder());
-        return auth;
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth)  {
-        auth.authenticationProvider(authenticationProvider());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(final HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .requestCache().disable()
                 .authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/", "/registration", "/login", "/bankAccount", "/contact",
-                        "/user", "/transfer", "/payment", "/recharge")
-                .permitAll();
+                .antMatchers("/login", "/registration").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().formLogin()
+                .loginProcessingUrl("/login") //the URL on which the clients should post the login information
+                .successHandler(this::loginSuccessHandler)
+                .failureHandler(this::loginFailureHandler)
+                .and()
+                .logout()
+                .logoutUrl("/logout") //the URL on which the clients should post if they want to logout
+                .logoutSuccessHandler(this::logoutSuccessHandler)
+                .invalidateHttpSession(true)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(this::authenticationEntryPointHandler);
+    }
+
+    private void loginSuccessHandler(HttpServletRequest request, HttpServletResponse response,
+                                     Authentication authentication) throws IOException {
+
+        response.setStatus(HttpStatus.OK.value());
+        objectMapper.writeValue(response.getWriter(), "You are now logged in to Pay my Buddy!");
+    }
+
+    private void loginFailureHandler(HttpServletRequest request, HttpServletResponse response,
+                                     AuthenticationException e) throws IOException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        objectMapper.writeValue(response.getWriter(), "The email or password you entered is incorrect.");
+    }
+
+    private void logoutSuccessHandler(HttpServletRequest request, HttpServletResponse response,
+                                      Authentication authentication) throws IOException {
+
+        response.setStatus(HttpStatus.OK.value());
+        objectMapper.writeValue(response.getWriter(), "You have successfully logged out!");
+    }
+
+    private void authenticationEntryPointHandler(HttpServletRequest request, HttpServletResponse response,
+                                                 AuthenticationException e) throws IOException {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        objectMapper.writeValue(response.getWriter(), "You have to logged in first!");
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    /**
+     * An AuthenticationProvider implementation that retrieves user details from
+     * a UserDetailsService.
+     *
+     * @return a DaoAuthenticationProvider object
+     */
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider =
+                new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
+
